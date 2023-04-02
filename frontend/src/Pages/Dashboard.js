879a8685866cf5b9d902428e679f19df4def561d
juli Gabani <juligabani@julis-MacBook-Pro.local>
2023-04-02 17:41:43 +1200

    Text Change

--- frontend/src/Pages/Dashboard.js
+++ frontend/src/Pages/Dashboard.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export class Dashboard extends Component {
     render() {
         return (
-            <h1>Dashboard Page!</h1>
+            <h1>Hey, This is a Dashboard Page!</h1>
         )
     }
 }
